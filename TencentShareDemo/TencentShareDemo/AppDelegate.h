7bd07da738610c89a58ab220abf741ec36287e46
李仁兵 <ziyoudelaoge@163.com>
2014-01-02 14:57:26 +0800

    提交

---
+++ TencentShareDemo/TencentShareDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TencentShareDemo
+//
+//  Created by 李仁兵 on 13-12-30.
+//  Copyright (c) 2013年 李仁兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TencentShareDemo/TencentShareDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TencentShareDemo
+//
+//  Created by 李仁兵 on 13-12-30.
+//  Copyright (c) 2013年 李仁兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
